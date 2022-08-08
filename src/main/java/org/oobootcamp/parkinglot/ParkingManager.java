06e3a7ca7d1d7e1cc7122b6e57dc1787a8beaac8
Xiaoming Gao <xiaoming.gao@rea-group.com>
2022-08-08 17:46:53 +0800

    refactor manager

--- src/main/java/org/oobootcamp/parkinglot/ParkingManager.java
+++ src/main/java/org/oobootcamp/parkinglot/ParkingManager.java
@@ -3,27 +3,38 @@ package org.oobootcamp.parkinglot;
 import org.oobootcamp.parkinglot.exception.InvalidTicketException;
 import org.oobootcamp.parkinglot.exception.ParkingLotFullException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.function.Predicate.not;
 
-public class ParkingManager {
-    private final List<Parkable> parkables;
+public class ParkingManager extends ParkingBoy {
+    private final List<ParkingBoy> parkingBoys;
 
-    public ParkingManager(List<Parkable> parkables) {
-        this.parkables = parkables;
+    public ParkingManager(List<ParkingLot> parkingLots, List<ParkingBoy> parkingBoys) {
+        super(parkingLots);
+        List<ParkingBoy> list = new ArrayList<>();
+        list.add(this);
+        list.addAll(parkingBoys);
+        this.parkingBoys = list;
     }
 
     public Ticket park(Car car) {
-        Parkable parkable = parkables.stream()
+        Parkable parkable = parkingBoys.stream()
                 .filter(not(Parkable::isFull))
                 .findFirst()
                 .orElseThrow(ParkingLotFullException::new);
         return parkable.park(car);
     }
 
+    @Override
+    protected Comparator<ParkingLot> parkingLotComparator() {
+        return (p1, p2) -> 0;
+    }
+
     public Car pickUp(Ticket ticket) {
-        Parkable parkable = parkables.stream()
+        Parkable parkable = parkingBoys.stream()
                 .filter(p -> p.containCarWith(ticket))
                 .findFirst()
                 .orElseThrow(InvalidTicketException::new);
